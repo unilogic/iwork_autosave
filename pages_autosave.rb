3a8e3ae0aa1a5395b0da496fd03607ad192ebea5
Ben Allen <ben@sysadminschronicles.com>
2008-10-09 13:06:53 -0400

    First commit, added blank readme and basic autosave script for Pages

---
+++ pages_autosave.rb
@@ -0,0 +1,15 @@
+#!/usr/local/bin/ruby
+
+require 'rubygems'
+require 'rbosa'
+
+pages = OSA.app('Pages')
+thread = Thread.new do
+  while true
+    pages.documents.each { |document| 
+      pages.save document 
+      }
+    sleep 120
+  end   
+end
+thread.join
